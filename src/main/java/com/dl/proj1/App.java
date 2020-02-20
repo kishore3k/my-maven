25902185233d96b15c5caa569899f73f3f23a938
root <root@ip-192-168-1-70.us-east-2.compute.internal>
2020-02-20 07:29:58 +0000

    web hook

---
+++ src/main/java/com/dl/proj1/App.java
@@ -0,0 +1,13 @@
+package com.dl.proj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "testing web hook" );
+    }
+}
