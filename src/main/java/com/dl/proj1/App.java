f4fc7d433c080babeb4e096365dcc7041e43bb85
kishore <kishore@gmail.com>
2020-02-20 09:18:24 +0000

    build on webhook

--- src/main/java/com/dl/proj1/App.java
+++ src/main/java/com/dl/proj1/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "this is sample project" );
+        System.out.println( "this is build on webhook");
+
     }
 }
